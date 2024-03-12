0b6c8d2f368d4f01abdeac545f99607bc01a890d
GustaafL <guus@seita.nl>
2024-03-12 16:09:43 +0100

    add rust formatter
    Signed-off-by: GustaafL <guus@seita.nl>

--- init.lua
+++ init.lua
@@ -611,6 +611,7 @@ require('lazy').setup({
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        rust = { 'rustfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
